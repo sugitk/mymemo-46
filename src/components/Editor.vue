e5d417166ad3d8ffa0b99548ab4df874e7285190
SUGIMURA Takashi <t@sugimura.cc>
2018-04-30 18:30:33 +0900

    initial commit

---
+++ src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="editor">
+      <h1>エディター画面</h1>
+      <span>{{ user.displayName }}</span>
+      <button @click="logout">ログアウト</button>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "editor",
+      data() {
+        return {
+        }
+      },
+      props: ['user'],
+      methods: {
+        logout: function() {
+          firebase.auth().signOut();
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">Google アカウントでログイン</button>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "home",
+      data () {
+        return {
+          msg: "mymemo46へようこそ"
+        }
+      },
+      methods: {
+        googleLogin: function() {
+          firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
